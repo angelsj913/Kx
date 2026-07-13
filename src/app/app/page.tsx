73a4d218ef361b98356e4412ed7c6e8464840255
angelsj913 <angelsj913@gmail.com>
2026-07-13 20:59:19 +0900

    Update page.tsx

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -6,10 +6,20 @@ export default function AppWorkspace() {
   return (
     <div className="flex h-screen w-full overflow-hidden bg-white">
       
-      {/* 왼쪽 사이드바 */}
-      <Sidebar />
+      {/* 사이드바 (임시로 빈 값 전달) */}
+      <Sidebar
+        sessions={[]}
+        activeSessionId={null}
+        activeView="chat"
+        onSelectSession={() => {}}
+        onNewChat={() => {}}
+        onDeleteSession={() => {}}
+        onOpenLibrary={() => {}}
+        onOpenReview={() => {}}
+        onOpenRag={() => {}}
+      />
 
-      {/* 오른쪽 메인 영역 (흰 배경만) */}
+      {/* 메인 영역 (흰 배경만) */}
       <div className="flex-1 min-w-0 bg-white flex items-center justify-center">
         <div className="text-center text-gray-400">
           <p className="text-lg">워크스페이스 영역</p>
